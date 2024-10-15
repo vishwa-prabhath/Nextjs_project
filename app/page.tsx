6832c94b114a557b66f7b449ccc0b9a108b337d8
Vishwa Gunathilake <vishwaprabhathgunathilake@gmail.com>
2024-10-15 21:13:27 +0530

    added Product list and add to cart
    Learned abt the csr and ssr. includes the button in add to cart pge and linked it with product page since the add to card has to be in client side.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
+import ProductCard from "./components/productCard";
 
 export default function Home() {
   return (
-    <main><h1>helllo world</h1></main>  );
+
+    <main>
+      <h1>helllo world</h1>
+      <Link href="/users">users</Link>
+      <ProductCard />
+      
+      </main>  );
 }
