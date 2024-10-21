4ea3d595d159845caae14a1944ec352102871bcc
Vishwa Gunathilake <vishwaprabhathgunathilake@gmail.com>
2024-10-21 10:38:02 +0530

    using tailwind instead css
    using this will help us to style component in the same place and since we use intelisence extension its easier .

--- app/components/productCard.tsx
+++ app/components/productCard.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import AddToCart from './AddToCart';
-import styles from './productCard.module.css';
+
 
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div className='p-5 my-5 bg-blue-400 text-white text-xl hover:bg-blue-500' >
         <AddToCart />
     </div>
   )
