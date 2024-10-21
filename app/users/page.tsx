c77a06cfe4eff9b03c162f270646d7f6d13a00d3
Vishwa Gunathilake <vishwaprabhathgunathilake@gmail.com>
2024-10-21 10:30:14 +0530

    css part

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,7 +7,7 @@ interface User {
 const userspage = async () => {
   const res = await fetch(
     'https://jsonplaceholder.typicode.com/users',
-    {cache:'no-store'});
+    {cache: 'no-store' });
   const users:User[] = await res.json();  //promise ekak nisa await dannwa . getting response
   
   return (
