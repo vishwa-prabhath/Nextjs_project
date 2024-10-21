254c9b488bd4b3532da6417c84af78c6cb7ef0db
Vishwa Gunathilake <vishwaprabhathgunathilake@gmail.com>
2024-10-21 10:56:18 +0530

    configuring daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,14 +6,11 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+  
+  plugins: [require('daisyui'),],
+  daisyui: {
+      themes: ["winter"],
     },
-  },
-  plugins: [],
-};
+  }
+
 export default config;
